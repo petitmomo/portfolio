9f9d7537bd4e9ac841492c083e118ca65d297e10
Momo <ba94.mamadou@gmail.com>
2025-10-24 16:48:14 +0000

    Edit du formulaire

--- src/composants/ServiceFormModal.jsx
+++ src/composants/ServiceFormModal.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { X } from "lucide-react";
-import emailjs from "emailjs-com";
+
 
 const ServiceFormModal = ({ isOpen, onClose }) => {
   const [formData, setFormData] = useState({
